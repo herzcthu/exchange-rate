e6a4d1284cb59d7e6028327967d9c58bb715fcb2
Sithu Thwin <sithu@thwin.net>
2017-08-20 15:25:43 +0630

    BotFactory initialize

--- app/Http/Controllers/ExChatBot.php
+++ app/Http/Controllers/ExChatBot.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 use Mpociot\BotMan\BotMan;
+use Mpociot\BotMan\BotManFactory;
 
 class ExChatBot extends Controller
 {
     public function autoreply()
     {
         Log::info(Request::all());
-        $botman = app('botman');
+        $config = config('services.botman');
+        $botman = BotManFactory::create($config);
 
         $botman->verifyServices(config('services.botman.chatbot_verify'));
         // Simple respond method
