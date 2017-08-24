c73ca767942c5a8c001b7609bb85d9cafabab3a5
Sithu Thwin <sithu@thwin.net>
2017-08-24 21:00:54 +0630

    who am I?

--- app/Http/Controllers/ExChatBot.php
+++ app/Http/Controllers/ExChatBot.php
@@ -20,14 +20,25 @@ class ExChatBot extends Controller
             $bot->reply('Hi there :)');
         });
 
-        $botman->hears("{name}", function (BotMan $bot, $name) {
+        $botman->hears('Who am I?', function($bot) {
+            $user = $bot->getUser();
+            $bot->reply('Hello '.$user->getFirstName().' '.$user->getLastName());
+            $bot->reply('Your username is: '.$user->getUsername());
+            $bot->reply('Your ID is: '.$user->getId());
+        });
+
+        $botman->hears("{message}([?\.၊။])", function (BotMan $bot, $message) {
             // Store information for the currently logged in user.
             // You can also pass a user-id / key as a second parameter.
-//            $bot->userStorage()->save([
-//                'name' => $name
-//            ]);
+            $bot->userStorage()->save([
+                'message' => $message
+            ]);
 
-            $bot->reply($name);
+            $bot->reply($message);
+        });
+
+        $botman->fallback(function($bot) {
+            $bot->reply('Sorry, I did not understand these commands.');
         });
 
         $botman->listen();
--- app/Http/Controllers/WebCrawlRate.php
+++ app/Http/Controllers/WebCrawlRate.php
@@ -6,6 +6,13 @@ use Herzcthu\ExchangeRates\CrawlBank;
 
 class WebCrawlRate extends Controller
 {
+    /**
+     * @param $service exrate
+     * @param $bank cbm, kbz, mcb, aya, agd, cbbank
+     * @param string $type sell, buy
+     * @param CrawlBank $crawlbank
+     * @return mixed JSON
+     */
     public function response($service, $bank, $type = 'sell', CrawlBank $crawlbank)
     {
         if($service == 'exrate') {
